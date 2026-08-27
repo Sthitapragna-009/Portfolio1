bb35799712dd8512984a934cd82a87f4f1e1ed09
KSP <kspmobi@gmail.com>
2026-08-27 23:14:54 +0530

    change 2

--- amrut-cupcb-redesign/src/components/PullQuote.jsx
+++
@@ -1,9 +0,0 @@
-export default function PullQuote({ text, attribution }) {
-  return (
-    <div className="pull-quote">
-      <span className="pull-quote__mark" aria-hidden="true">“</span>
-      <p>{text}</p>
-      {attribution && <span className="pull-quote__attribution">{attribution}</span>}
-    </div>
-  );
-}
